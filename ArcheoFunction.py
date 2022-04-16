390a845f0f53dec06bf3b2693c4b1138644f8b1e
Sirine86 <ghsirin@gmail.com>
2022-04-16 15:36:04 +0300

    Update ArcheoFunction.py

--- ArcheoFunction.py
+++ ArcheoFunction.py
@@ -1,3 +1,31 @@
 import geopandas as gpd
 import matplotlib.lines as mlines
 import cartopy.crs as ccrs
+import sys
+print(sys.version) ## define system version of python
+
+# generate matplotlib handles to create a legend of the features we put in our map.
+def generate_handles(labels, colors, edge='k', alpha=1):
+    lc = len(colors)  # get the length of the color list
+    handles = []
+    for i in range(len(labels)):
+        handles.append(mpatches.Rectangle((0, 0), 1, 1, facecolor=colors[i % lc], edgecolor=edge, alpha=alpha))
+    return handles
+
+def scale_bar(ax, location=(0.92, 0.95)):
+    llx0, llx1, lly0, lly1 = ax.get_extent(ccrs.PlateCarree())
+    sbllx = (llx1 + llx0) / 2
+    sblly = lly0 + (lly1 - lly0) * location[1]
+
+    tmc = ccrs.TransverseMercator(sbllx, sblly)
+    x0, x1, y0, y1 = ax.get_extent(tmc)
+    sbx = x0 + (x1 - x0) * location[0]
+    sby = y0 + (y1 - y0) * location[1]
+
+    plt.plot([sbx, sbx - 20000], [sby, sby], color='k', linewidth=9, transform=tmc)
+    plt.plot([sbx, sbx - 10000], [sby, sby], color='k', linewidth=6, transform=tmc)
+    plt.plot([sbx-10000, sbx - 20000], [sby, sby], color='w', linewidth=6, transform=tmc)
+
+    plt.text(sbx, sby-4500, '20 km', transform=tmc, fontsize=8)
+    plt.text(sbx-12500, sby-4500, '10 km', transform=tmc, fontsize=8)
+    plt.text(sbx-24500, sby-4500, '0 km', transform=tmc, fontsize=8)
